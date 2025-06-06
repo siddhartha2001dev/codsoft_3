60b711efa167a8082dd487725c57e1f619cbb8d8
Siddhartha Banerjee <siddhubj31@gmail.com>
2025-06-06 22:41:34 +0530

    Add files via upload

---
+++ Calculator/js/script.js
@@ -0,0 +1,22 @@
+
+let display = document.getElementById("display");
+
+function appendValue(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
